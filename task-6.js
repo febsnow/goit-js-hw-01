367461b3e686045e9bf770f20c30e09948345f97
Smekalin Sergei <crypto.mnr@gmail.com>
2020-08-17 10:58:13 +0300

    Update task-6.js

--- task-6.js
+++ task-6.js
@@ -17,3 +17,17 @@ do {
   }
 } while (input !== null);
 alert(`Общая сумма введенных чисел ${total}`);
+
+/*let input;
+let total = 0;
+input = prompt('Введите число', 'только число');
+Number(input);
+while (input) {
+  if (isNaN(input)) {
+    input = prompt('Было введено не число, попробуйте еще раз');
+  } else {
+    total += Number(input);
+    input = prompt('Введите число', 'только число');
+  }
+}
+alert(`Общая сумма чисел равна: ${total}`);*/
